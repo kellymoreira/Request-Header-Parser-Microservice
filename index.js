d1a13277050b6cb4a15ca1db4f9e410719d923ee
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2024-02-03 20:56:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static("public"));
 
