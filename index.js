963ab3c0539a1a30598067be7c1fe1f5c5e4b9e1
Kelly Cristina Moreira <kelly.moreira0510@gmail.com>
2024-02-03 20:55:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
-// Where the node app starts
-
-// init project
 require("dotenv").config();
 var express = require("express");
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Middleware to extract IP address, language, and software
 app.use((req, res, next) => {
   const ipaddress = req.ip;
   const language = req.headers["accept-language"];
   const software = req.headers["user-agent"];
 
-  // Create the response JSON object
   const responseObj = {
     ipaddress,
     language,
     software,
   };
 
-  // Send the JSON response
   res.json(responseObj);
 });
 
-// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-// listen for requests
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
